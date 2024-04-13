93dfe20057577a2fbf91bc13d8255ca1f71a91e9
Oscar Mesa <oscar.mesa@chiper.co>
2024-04-12 19:44:49 -0500

    consultar todos los estudiantes

--- src/main/java/co/edu/poli/ces3/universitas/database/ConexionMySql.java
+++ src/main/java/co/edu/poli/ces3/universitas/database/ConexionMySql.java
@@ -1,8 +1,6 @@
 package co.edu.poli.ces3.universitas.database;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class ConexionMySql {
 
@@ -21,7 +19,7 @@ public class ConexionMySql {
         nameDatabase = "ces3-universitas";
     }
 
-    public Connection createConexion(){
+    public void createConexion(){
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             cnn = DriverManager.getConnection(
@@ -32,12 +30,37 @@ public class ConexionMySql {
             System.out.println("An error occurred during the connection");
             throw new RuntimeException(e);
         }
-        return null;
+    }
+
+    public void getUsers() throws SQLException {
+        String sql = "SELECT * FROM users";
+        String[] nameColumns = {"id", "name", "lastName", "mail", "password", "createdAt", "updatedAt", "deletedAt"};
+        try {
+            createConexion();
+            Statement stmt = cnn.createStatement();
+            ResultSet result = stmt.executeQuery(sql);
+            while(result.next()){
+                for (String field: nameColumns) {
+                    System.out.println("************");
+                    System.out.println("Name Column: " + result.getString(field));
+                    System.out.println("*************");
+                }
+            }
+            stmt.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }finally {
+            cnn.close();
+        }
     }
 
     public static void main(String[] args) {
         ConexionMySql conection = new ConexionMySql();
-        conection.createConexion();
+        try {
+            conection.getUsers();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
